4cf3a9af615f8230a009598b4cba3ede7a3f5640
Virtuso1225 <dominico1225@gmail.com>
2021-07-11 11:44:46 +0900

    Login.Dev

--- components/front/Front.js
+++ components/front/Front.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import { Text, View, Button } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import Icon from 'react-native-vector-icons/Entypo';
+
 import {
-  ButtonWrapper,
   HeadTitle,
-  InputContent,
   SubTitle,
-  TextWrapper,
   TitleWrapper,
   InputWrapper,
   Input,
   StyledButton,
   BottomWrapper,
+  CheckWrapper,
+  Check,
+  Description,
+  TextArea,
 } from './FrontStyle';
 
 const Front = ({ navigation }) => {
@@ -20,28 +24,92 @@ const Front = ({ navigation }) => {
         <HeadTitle>안암학사</HeadTitle>
         <SubTitle>고려대학교</SubTitle>
       </TitleWrapper>
+      <TextArea>
+        <LinearGradient
+          colors={['#E8EBF2', '#F2F3F7']}
+          // colors={['red', 'yellow', 'green']}
+          style={styles.TextWrapper}
+          start={{ x: 0.7, y: 0 }}
+          end={{ x: 1, y: 0 }}
+        >
+          <InputWrapper>
+            <Input placeholder="user ID (학번)" />
+          </InputWrapper>
+        </LinearGradient>
+        <LinearGradient
+          colors={['#E8EBF2', '#F2F3F7']}
+          style={styles.TextWrapper}
+          start={{ x: 0.7, y: 0 }}
+          end={{ x: 1, y: 0 }}
+        >
+          <InputWrapper>
+            <Input placeholder="password" />
+          </InputWrapper>
+        </LinearGradient>
+        <CheckWrapper>
+          <Check>
+            <Icon name="check" size={25} color="#707070" />
+          </Check>
+          <Description>Keep me logged in</Description>
+        </CheckWrapper>
+      </TextArea>
       <BottomWrapper>
-        <TextWrapper>
-          <InputWrapper>
-            <InputContent>user ID (학번)</InputContent>
-            <Input />
-          </InputWrapper>
-          <InputWrapper>
-            <InputContent>password</InputContent>
-            <Input />
-          </InputWrapper>
-        </TextWrapper>
-        <ButtonWrapper>
+        <LinearGradient
+          colors={['#E8EBF2', '#F2F3F7']}
+          style={styles.ButtonWrapper}
+          start={{ x: 0.7, y: 0 }}
+          end={{ x: 1, y: 0 }}
+        >
           <StyledButton onPress={() => navigation.navigate('Login')}>
             Login
           </StyledButton>
+        </LinearGradient>
+        <LinearGradient
+          colors={['#E8EBF2', '#F2F3F7']}
+          style={styles.ButtonWrapper}
+          start={{ x: 0.7, y: 0 }}
+          end={{ x: 1, y: 0 }}
+        >
           <StyledButton onPress={() => navigation.navigate('Register')}>
             Register
           </StyledButton>
-        </ButtonWrapper>
+        </LinearGradient>
       </BottomWrapper>
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  TextWrapper: {
+    borderRadius: 10,
+    minWidth: 301,
+    height: 54,
+    justifyContent: 'space-around',
+    alignSelf: 'center',
+    paddingTop: 3,
+    paddingLeft: 10,
+    paddingBottom: 3,
+    paddingRight: 10,
+    marginTop: 29,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
+    elevation: 3,
+  },
+  ButtonWrapper: {
+    alignItems: 'center',
+    marginTop: 30,
+    borderRadius: 10,
+    width: 140,
+    height: 39,
+    marginRight: 10,
+    marginLeft: 10,
+    justifyContent: 'center',
+  },
+});
 
 export default Front;
--- components/front/FrontStyle.js
+++ components/front/FrontStyle.js
@@ -1,78 +1,83 @@
 import styled from 'styled-components/native';
 
 export const TitleWrapper = styled.View`
-  flex: 1;
   display: flex;
-  flex-direction: column;
   border: none;
   align-items: center;
-  padding-top: 100px;
+  margin-top: 89px;
 `;
 
 export const HeadTitle = styled.Text`
   padding-bottom: 10px;
-  font-size: 50px;
-  color: gray;
+  font-size: 58px;
+  color: rgba(133, 0, 0, 1);
 `;
 
 export const SubTitle = styled.Text`
-  font-size: 25px;
-  color: gray;
+  font-size: 28px;
+  color: black;
 `;
 
-export const BottomWrapper = styled.View`
-  display: flex;
-  margin-bottom: 50px;
-`;
-export const TextWrapper = styled.View`
-  display: flex;
-  background-color: lightgray;
-  border: none;
-  border-radius: 12px;
-  min-width: 200px;
-  height: 50px;
-  justify-content: space-around;
+export const TextArea = styled.View`
+  margin-top: 150px;
+  width: 302px;
   align-self: center;
-  padding: 3px 10px 3px 10px;
 `;
 
 export const InputWrapper = styled.View`
   display: flex;
   flex-direction: row;
-  justify-content: flex-end;
-`;
-
-export const InputContent = styled.Text`
-  font-size: 10px;
 `;
 
 export const Input = styled.TextInput`
-  font-size: 10px;
+  font-size: 19px;
   margin-left: 15px;
   padding-left: 4px;
-  border-bottom-color: rgba(0, 0, 0, 0.5);
-  border-bottom-width: 1px;
   min-width: 80px;
-  align-content: flex-end;
+`;
+
+export const CheckWrapper = styled.View`
+  display: flex;
+  flex-direction: row;
+  margin-top: 9px;
+  align-items: flex-start;
+`;
+
+export const Check = styled.View`
+  display: flex;
+  background-color: white;
+  width: 25px;
+  height: 25px;
+  border-radius: 5px;
+  margin-right: 11px;
+  align-content: center;
+  justify-content: center;
+`;
+
+export const Description = styled.Text`
+  font-size: 14px;
+`;
+
+export const BottomWrapper = styled.View`
+  display: flex;
+  margin-bottom: 50px;
+  flex-direction: row;
+  align-self: center;
 `;
 
 export const ButtonWrapper = styled.View`
-  display: flex;
+  align-items: center;
+  margin-top: 30px;
+  background-color: lightgray;
+  border-radius: 12px;
+  width: 140px;
+  height: 39px;
+  margin-right: 10px;
+  margin-left: 10px;
   justify-content: center;
-  flex-direction: row;
-  margin-top: 8px;
 `;
 
 export const StyledButton = styled.Text`
-  display: flex;
-  padding-left: 2.5px;
-  padding-right: 2.5px;
-  margin-left: 2.5px;
-  margin-right: 2.5px;
   font-size: 15px;
-  color: black;
-  background-color: lightgray;
-  min-width: 80px;
-  justify-content: center;
-  text-align: center;
+  color: #850000;
 `;
